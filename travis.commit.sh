a4a0bd4fdff54afe51e83eb11ea34e40deb5c8c1
Ryan Fleck <ryan.fleck@protonmail.com>
2018-12-21 10:06:55 -0700

    CI/CD Troubleshooting.

--- travis.commit.sh
+++ travis.commit.sh
@@ -5,23 +5,20 @@ echo "Pushing updated HUGO site to GitHub."
 setup_git() {
   git config user.email "travis@travis-ci.org"
   git config user.name "Travis CI"
+  
+  git remote rm origin
+  git remote add origin https://Travis-CI:${GH_TOKEN}@github.com/RyanFleck/hugo-test.git
+  git checkout master
 }
 
 commit_website_files() {
   git add .
   echo "Commit build $TRAVIS_BUILD_NUMBER"
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-}
-
-upload_files() {
-  echo "Push files to master."
-  git remote rm origin
-  git remote add origin https://Travis-CI:${GH_TOKEN}@github.com/RyanFleck/hugo-test.git
   git push 
 }
 
 setup_git
 commit_website_files
-upload_files
 
 exit 0
