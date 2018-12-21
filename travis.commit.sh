b02fce6acec45aa304cf029d89ae4adcec59bd58
Ryan Fleck <ryan.fleck@protonmail.com>
2018-12-21 12:51:54 -0700

    Update travis script.

--- travis.commit.sh
+++ travis.commit.sh
@@ -2,23 +2,11 @@
 
 echo "Pushing updated HUGO site to GitHub."
 
-setup_git() {
-  git config user.email "travis@travis-ci.org"
-  git config user.name "Travis CI"
-  
-  git remote rm origin
-  git remote add origin https://Travis-CI:${GH_TOKEN}@github.com/RyanFleck/hugo-test.git
-  git checkout master
-  git push --set-upstream origin master 
-}
-
-commit_website_files() {
-  git add .
-  echo "Commit build $TRAVIS_BUILD_NUMBER"
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER [ci skip]"
-}
-
-commit_website_files
-setup_git
+git config user.email "travis@travis-ci.org"
+git config user.name "Travis CI"
+git add docs/* 
+echo "Commit build $TRAVIS_BUILD_NUMBER"
+git commit --allow-empty -m "Travis build: $TRAVIS_BUILD_NUMBER [ci skip]"
+git push https://RyanFleck:${GH_TOKEN}@github.com/RyanFleck/hugo-test.git HEAD:master
 
 exit 0
